4922ecf52e39defbcb6cb3b8c2b929e2749a4205
default <yunus.emre.guzel@mths.ca>
2025-03-31 21:20:11 +0000

    added the templete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: March 27  2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function volumeOfPyramid() {
+  // Setting the varibles
+  const LENGTH_Of_PYRAMID = parseFloat(document.getElementById("length-of-pyramid").value)
+  const WIDTH_OF_PYRAMID = parseFloat (document.getElementById("width-of-pyramid").value)
+  const HEIGHT_OF_PYRAMID = parseFloat (document.getElementById("height-of-pyramid").value)
+  const volumeOfPyramid = (baseAOfTrapizoid + baseBOfTrapizoid) /3
+  // clacualtoins 
+  document.getElementById("area").innerHTML ="Area is: " + volumeOfPyramid + "mm²"
+}
