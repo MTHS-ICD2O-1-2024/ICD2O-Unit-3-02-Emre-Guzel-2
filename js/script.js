b035163c011684d2454901f595f66424c6c65d6e
default <yunus.emre.guzel@mths.ca>
2025-04-06 00:39:38 +0000

    fixed style of script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@
 
 function volumeOfPyramid() {
   // Setting the varibles
-  const LENGTH_Of_PYRAMID = parseFloat(document.getElementById("length-of-pyramid").value)
-  const WIDTH_OF_PYRAMID = parseFloat(document.getElementById("width-of-pyramid").value)
-  const HEIGHT_OF_PYRAMID = parseFloat(document.getElementById("height-of-pyramid").value)
-  const volumeOfPyramid = (LENGTH_Of_PYRAMID * WIDTH_OF_PYRAMID * HEIGHT_OF_PYRAMID) / 3
+  const lengthOfPyramid = parseFloat(document.getElementById("length-of-pyramid").value)
+  const widthOfPyramid = parseFloat(document.getElementById("width-of-pyramid").value)
+  const heightOfPyramid = parseFloat(document.getElementById("height-of-pyramid").value)
+  const volumeOfPyramid = (lengthOfPyramid* widthOfPyramid * heightOfPyramid) / 3
   // clacualtoins 
   document.getElementById("Volume").innerHTML = 
   "Volume is: " + volumeOfPyramid.toFixed(2) + " mm³";
